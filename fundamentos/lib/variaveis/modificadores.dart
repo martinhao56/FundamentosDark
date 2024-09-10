75c0a1e112b7c8c1555dcb54cbb835aac311ad1a
leo <martinhaoleonardo252@gmail.com>
2024-09-10 20:30:22 -0300

    variaveis

---
+++ fundamentos/lib/variaveis/modificadores.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+  
+  var nomeCurso ="Fundamentos Dart";
+  print(nomeCurso);
+
+nomeCurso = "Fundamento em Dart versão atual";
+print(nomeCurso);
+
+
+//final
+// uma variavel declarada com inal pode ser atribuido apenas uma vez e seu valor não pode
+// ser altera depis de ser definido
+final nomeCursoFinal = "Fundamentos em Dart";
+print(nomeCursoFinal);
+
+// não e possivel reatribuir o valor no varável declarada como final
+// nomeCursofinal - "fundamentos em dart atual"
+
+ const nomeCursoConstante = "fundamentos em Dart";
+print(nomeCursoConstante);
+
+nomeCursoConstante = "fundamentos em dart versão final";
+
+//Const
+const nomeCursoConstante = "Fundamento em Dart";
+print(nomeCursoConstante);
+
+
+
+
+
+}
