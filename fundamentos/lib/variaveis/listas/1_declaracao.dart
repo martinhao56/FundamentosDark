421f04925f75702b5da15e3c0c43bcd0c549c898
leo <martinhaoleonardo252@gmail.com>
2024-09-10 22:17:09 -0300

    listas

---
+++ fundamentos/lib/variaveis/listas/1_declaracao.dart
@@ -0,0 +1,19 @@
+void main(List<String> args){
+
+  //Declaração e inicialização de uma lista de interios
+  //Tipo Genérios
+ List<int> listaNumeros = [1,2,3];
+
+ List<String> listaTexto = ['Fulano','Beltrano','sicrano'];
+
+ print(listaTexto[1]);
+ print(listaNumeros[1]);
+
+ List<int> listaSemDadosGenerico = [];
+
+ var listaSemDadosInferencia = [];
+
+ varlistaSemDadosInferenciaGenerio = <int>[];
+
+ var listaSemDadosInferenciaGenericosString = <string>[];
+}
